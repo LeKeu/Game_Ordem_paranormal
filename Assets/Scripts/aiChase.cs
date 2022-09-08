b05422ef60c632ad3ea3ddb51e0d1dff69d03371
LeKeu <101370021+LeKeu@users.noreply.github.com>
2022-09-08 19:38:21 -0300

    dog fast animacao

--- Assets/Scripts/aiChase.cs
+++ Assets/Scripts/aiChase.cs
@@ -7,11 +7,13 @@ public class aiChase : MonoBehaviour
     GameObject player;
     public float velInimigo;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     float dist;
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");    
     }
@@ -19,10 +21,22 @@ public class aiChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         rb.drag = 20;
         dist = Vector2.Distance(transform.position, player.transform.position);
         //Vector2 direc = player.transform.position - transform.position;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, velInimigo * Time.deltaTime);
+
+        if (player.gameObject.transform.position.x > transform.position.x)
+        {
+            Debug.Log("player na direita");
+            sr.flipX = true;
+        }
+        else
+        {
+            Debug.Log("player na esquerda");
+            sr.flipX = false;
+        }
     }
 }
