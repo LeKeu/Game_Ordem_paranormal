6ac012903302dca06a8b6db9c6dce7872f348080
LeKeu <lekeuffer@gmail.com>
2022-09-13 20:17:45 -0300

    selec personagem

---
+++ Assets/Scripts/menu_buts.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_buts : MonoBehaviour
+{
+    public void PersonScreen()
+    {
+        SceneManager.LoadScene("Select_personagem");
+    }
+}
